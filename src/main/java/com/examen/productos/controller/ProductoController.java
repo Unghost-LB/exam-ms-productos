e5e07d77d8102fd4cd66c055ab446e1bc885cae3
Daniela <crisang15243@gmail.com>
2026-05-28 15:13:39 -0500

    feat: add CrossOrigin to controllers

--- src/main/java/com/examen/productos/controller/ProductoController.java
+++ src/main/java/com/examen/productos/controller/ProductoController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/productos")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class ProductoController {
 
     private final ProductoService service;
